61050291eed8fe37bcaa5d072cddc2c35cc3aa56
Joy Wang <w.joy5015@gmail.com>
2019-11-25 14:40:42 -0500

    feat: tests in models and routes written

--- capstone-database/server/db/models/museum.js
+++ capstone-database/server/db/models/museum.js
@@ -4,6 +4,10 @@ const db = require('../db')
 const Museum = db.define('museum', {
   name: {
     type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    }
   },
   location: {
     type: Sequelize.STRING
