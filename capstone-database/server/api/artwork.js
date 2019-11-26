6cd3ae89a4338ce6742e43b8051b8973bbc29f22
Joy Wang <w.joy5015@gmail.com>
2019-11-26 11:43:10 -0500

    added post and put routers to artwork

--- capstone-database/server/api/artwork.js
+++ capstone-database/server/api/artwork.js
@@ -23,4 +23,48 @@ router.get("/:id", async (req, res, next) => {
   }
 })
 
+router.post("/", async (req, res, next) => {
+  try{
+    const newArt = {
+      name: req.body.name,
+      artist: req.body.artist,
+      date: req.body.date,
+      imageUrl: req.body.imageUrl,
+      videoUrl: req.body.videoUrl,
+      description: req.body.description,
+      descriptionSound: req.body.descriptionSound
+    }
+    await Artwork.create(newArt)
+    const createdArt = await Artwork.findAll({
+      where: {
+        name: req.body.name,
+        artist: req.body.artist
+      }}
+      )
+    res.json(createdArt)
+  }catch(err){
+    next(err)
+  }
+})
+
+router.put("/:id", async (req, res, next) => {
+  try{
+    const currentArt = await Artwork.findByPk(req.params.id)
+    const newProperties = {
+      name: req.body.name,
+      artist: req.body.artist,
+      date: req.body.date,
+      imageUrl: req.body.imageUrl,
+      videoUrl: req.body.videoUrl,
+      description: req.body.description,
+      descriptionSound: req.body.descriptionSound,
+      museumId: req.body.museumId
+    }
+    await currentArt.update(newProperties)
+    res.json(currentArt)
+  }catch(err){
+    next(err)
+  }
+})
+
 module.exports = router
