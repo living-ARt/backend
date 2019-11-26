e42a68f69f9738d965520f430982b80a21150e0b
Joy Wang <w.joy5015@gmail.com>
2019-11-26 15:46:10 -0500

    routes for museum

--- server/api/museum.js
+++ server/api/museum.js
@@ -10,4 +10,37 @@ router.get("/:id", async (req, res, next) => {
   }
 })
 
+router.post("/", async (req, res, next) => {
+  try{
+    const newMuseum = {
+      name: req.body.name,
+      location: req.body.location
+    }
+    await Museum.create(newMuseum)
+    const createdMuseum = await Museum.findAll({
+      where: {
+        name: req.body.name,
+        location: req.body.location
+      }
+    })
+    res.json(createdMuseum)
+  }catch(err){
+    next(err)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try{
+    const currentMuseum = await Museum.findByPk(req.params.id)
+    const newProperties = {
+      name: req.body.name,
+      location: req.body.location
+    }
+    await currentMuseum.update(newProperties)
+    res.json(currentMuseum)
+  }catch(err){
+    next(err)
+  }
+})
+
 module.exports = router
